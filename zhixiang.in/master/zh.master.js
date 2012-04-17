d34a326dfc98594baf43c1d6f454b284c3f0da3d
Michael Yin <layerssss@gmail.com>
2012-04-18 00:21:07 +0800

    Update zhixiang.in/master/zh.master.js

--- zhixiang.in/master/zh.master.js
+++ zhixiang.in/master/zh.master.js
@@ -87,8 +87,10 @@ if (typeof (arguments[0].head) == 'function') { arguments[0].head(); } /*-->
 arguments[0].body(); /*-->
             <hr>
             <footer>
-        <p>&copy; 尹志翔 2012 由<a href="http://twitter.github.com/bootstrap/" target="_blank">Bootstrap</a>、
-        <a href="https://github.com/layerssss/ispJs" target="_blank">ISP.js</a>强力驱动
+        <p>&copy; 尹志翔 2012 感谢拯救我于水深火热之中的
+        <a href="http://twitter.github.com/bootstrap/" target="_blank">Bootstrap</a>,
+        <a href="https://github.com/layerssss/ispJs" target="_blank">ISP.js</a>
+        <a href="http://pjax.heroku.com/" target="_blank">pjax</a>
         </p>
       </footer>
         </div>
