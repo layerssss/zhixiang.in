e6a7dc6b6bd91f1a22b99c9fbf2abe00bfd8db37
Michael Yin <layerssss@gmail.com>
2012-05-03 15:10:48 +0800

    content update

--- zhixiang.in/master/eo.master.js
+++ zhixiang.in/master/eo.master.js
@@ -9,7 +9,7 @@ var misc = $load('inc/misc.inc.js')();
     <title>
         <!--*/
 if (typeof (arguments[0].title) != 'undefined') { $((arguments[0].title ? arguments[0].title : 'ne tilola') + '-'); } /*-->
-        Mikelo</title>
+        ĝieŝo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
