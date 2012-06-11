fe1178942c6399e32ca9813edc2058ea36b47618
Michael Yin <layerssss@gmail.com>
2012-06-11 13:29:51 +0800

    fix title bug

--- zhixiang.in/master/eo.master.js
+++ zhixiang.in/master/eo.master.js
@@ -8,7 +8,7 @@ var misc = $load('inc/misc.inc.js')();
     <meta charset="utf-8">
     <title>
         <!--*/
-if (typeof (arguments[0].title) != 'undefined') { $((arguments[0].title ? arguments[0].title : 'ne tilola') + '-'); } /*-->
+if (typeof (arguments[0].title) != 'undefined' && arguments[0].title) { $((arguments[0].title ? arguments[0].title : 'no titila') + '-'); } /*-->
         Mikelo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
